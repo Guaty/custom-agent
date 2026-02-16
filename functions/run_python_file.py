04dc9313122529da2e260e79511d455f1d6cce70
Guaty <bjarocha911@gmail.com>
2026-02-15 23:26:23 -0600

    Add run_python_file function

---
+++ functions/run_python_file.py
@@ -0,0 +1,37 @@
+import os
+import subprocess
+
+
+def run_python_file(working_directory, file_path, args=None):
+    try:
+        abs_working_dir = os.path.abspath(working_directory)
+        target_path = os.path.normpath(os.path.join(abs_working_dir, file_path))
+
+        if os.path.commonpath([abs_working_dir, target_path]) != abs_working_dir:
+            return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+        if not os.path.isfile(target_path):
+            return f'Error: "{file_path}" does not exist or is not a regular file'
+        if not target_path.endswith(".py"):
+            return f'Error: "{file_path}" is not a Python file'
+        
+        command = ["python", target_path]
+        if args is not None:
+            command.extend(args)
+
+        completed_process = subprocess.run(command, capture_output=True, text=True, timeout=30)
+
+        output_str = []
+        if completed_process.returncode != 0:
+            output_str.append(f"Process exited with code {completed_process.returncode}")
+        if completed_process.stdout is None and completed_process.stderr is None:
+            output_str.append("No output produced")
+        else:
+            if completed_process.stdout:
+                output_str.append(f"STDOUT: {completed_process.stdout}")
+            if completed_process.stderr:
+                output_str.append(f"STDERR: {completed_process.stderr}")
+        
+        return "\n".join(output_str)
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
+    
